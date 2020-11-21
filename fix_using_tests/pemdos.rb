a511eb740cd1e6e517896dc9589c477c55bf8b88
James DeSousa <james.desousa45@gmail.com>
2020-11-21 21:28:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,13 +1,10 @@
 require 'pry'
 def snake_it_up(string)
-  count = 0
+ 
   
   if string[0] == "s"
-    while count < 10 do
-      count = count + 1
-      binding.pry 
+    string = "ssssssssss" + string 
     end
-    string = newstring + string 
   else
     string
   end
